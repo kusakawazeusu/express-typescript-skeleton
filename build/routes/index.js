c225c4f7f0e44bcc787794dc21579040321108d4
kungtinglin <kusakawazeusu@universetech.cc>
2019-09-24 22:32:09 +0800

    delete build folder

--- build/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.send('index');
-});
-exports.default = router;
